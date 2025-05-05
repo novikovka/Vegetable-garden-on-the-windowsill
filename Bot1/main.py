121b3addd1f6c1950355afe4ad46e1ad915c568a
novikovka <a.novikovka@yandex.ru>
2025-05-05 17:25:24 +0500

    added section "notifications"

--- Bot1/main.py
+++ Bot1/main.py
@@ -3,8 +3,8 @@ from telebot import types
 import json
 import os
 
-bot = telebot.TeleBot('7634133767:AAF_vQNs8f-eu3Ec6aYVlG7ndkXKyluYoI4') #тестовый бот
-#bot = telebot.TeleBot('7789381064:AAFFdBdqwiNBJrq16UExKKLiprnDpCpRACo') #чистовик
+#bot = telebot.TeleBot('7634133767:AAF_vQNs8f-eu3Ec6aYVlG7ndkXKyluYoI4') #тестовый бот
+bot = telebot.TeleBot('7789381064:AAFFdBdqwiNBJrq16UExKKLiprnDpCpRACo') #чистовик
 
 STATE_FILE = "state.json"
 
@@ -27,19 +27,30 @@ def save_states():
     with open(STATE_FILE, "w") as f:
         json.dump(user_states, f, indent=4)
 
-@bot.message_handler(commands=["start"])
-def start(message):
-    user_id = str(message.chat.id)
-    if user_id not in user_states:
-        user_states[user_id] = {"light": False, "watering": False, "actions": {}}
-        save_states()
+@bot.message_handler(commands=["settings"])
+def settings_command(message):
+   user_id = str(message.chat.id)
+   if user_id not in user_states:
+       user_states[user_id] = {"light": False, "watering": False, "actions": {}, "notifications": {}}
+       save_states()
+   send_settings(message)  
 
-    bot.send_message(message.chat.id, f'Здравствуйте, {message.from_user.first_name}!')
-    markup = types.InlineKeyboardMarkup()
-    markup.add(types.InlineKeyboardButton('Настройки системы', callback_data='settings'))
-    markup.add(types.InlineKeyboardButton('Показания датчиков', callback_data='sensors'))
-    markup.add(types.InlineKeyboardButton('Настройки уведомлений', callback_data='notifications'))
-    bot.send_message(message.chat.id, 'Выберите пункт меню:', reply_markup=markup)
+@bot.message_handler(commands=["sensors"])
+def sensors_command(message):
+   user_id = str(message.chat.id)
+   if user_id not in user_states:
+       user_states[user_id] = {"light": False, "watering": False, "actions": {}, "notifications": {}}
+       save_states()
+   send_controls(message)  
+
+@bot.message_handler(commands=["notifications"])
+def notifications_command(message):
+   user_id = str(message.chat.id)
+   if user_id not in user_states:
+       user_states[user_id] = {"light": False, "watering": False, "actions": {}, "notifications": {}}
+       save_states()
+   send_notifications(message)  
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def handle_callback(call):
@@ -53,7 +64,8 @@ def handle_callback(call):
     elif call.data == 'sensors':
         send_controls(call.message)
     elif call.data == 'notifications':
-        bot.send_message(call.message.chat.id, '<em>уведомления</em>', parse_mode='html')
+        send_notifications(call.message)
+        #bot.send_message(call.message.chat.id, '<em>уведомления</em>', parse_mode='html')
     elif call.data == 'light_on':
         user_states[user_id]["light"] = True
         bot.answer_callback_query(call.id, "Свет включен 💡")
@@ -82,6 +94,23 @@ def handle_callback(call):
     elif call.data == 'custom_button_4':
         user_input_steps[user_id] = {"step": 7}
         bot.send_message(call.message.chat.id, "Введите номер действия:")
+    elif call.data == 'custom_1': #добавить триггер
+        add_notifications_buttons(call.message)
+    elif call.data == 'custom_2': #удалить существующий триггер
+        user_input_steps[user_id] = {"step": 30}
+        bot.send_message(call.message.chat.id, "Введите номер триггера:")
+    elif call.data == 'custom_11':
+        user_input_steps[user_id] = {"step": 8}
+        bot.send_message(call.message.chat.id, "Введите значение параметра 'влажность' при котором Вы хотите получать уведомление:")
+    elif call.data == 'custom_12':
+        user_input_steps[user_id] = {"step": 9}
+        bot.send_message(call.message.chat.id, "Введите значение параметра 'температура' при котором Вы хотите получать уведомление:")
+    elif call.data == 'custom_13':
+        user_input_steps[user_id] = {"step": 10}
+        bot.send_message(call.message.chat.id, "Введите значение параметра 'освещенность' при котором Вы хотите получать уведомление:")
+    elif call.data == 'custom_14':
+        user_input_steps[user_id] = {"step": 11}
+        bot.send_message(call.message.chat.id, "Введите значение параметра 'уровень воды' при котором Вы хотите получать уведомление:")
 
     save_states()
 
@@ -111,7 +140,6 @@ def handle_user_input(message):
         except ValueError:
             # В случае ошибки просим ввести корректное значение
             bot.send_message(message.chat.id, "Пожалуйста, введите корректный час (от 0 до 23).")
-
     # Шаг 2: обработка ввода минут
     elif step_info["step"] == 2:  # добавляем минуты в файл
         try:
@@ -144,7 +172,6 @@ def handle_user_input(message):
         except ValueError:
             # В случае ошибки просим ввести корректное значение
             bot.send_message(message.chat.id, "Пожалуйста, введите корректную минуту (от 0 до 59).")
-
     elif step_info["step"] == 3:
         try:
             # Пытаемся преобразовать текст сообщения в число (час)
@@ -191,7 +218,6 @@ def handle_user_input(message):
         except ValueError:
             # В случае ошибки просим ввести корректное значение
             bot.send_message(message.chat.id, "Пожалуйста, введите корректную минуту (от 0 до 59).")
-
     elif step_info["step"] == 5:  # добавляем часы в файл
         try:
             # Пытаемся преобразовать текст сообщения в число (час)
@@ -207,7 +233,6 @@ def handle_user_input(message):
         except ValueError:
             # В случае ошибки просим ввести корректное значение
             bot.send_message(message.chat.id, "Пожалуйста, введите корректный час (от 0 до 23).")
-
     elif step_info["step"] == 6:
         try:
             # Пытаемся преобразовать текст сообщения в число (минуты)
@@ -239,7 +264,6 @@ def handle_user_input(message):
         except ValueError:
             # В случае ошибки просим ввести корректное значение
             bot.send_message(message.chat.id, "Пожалуйста, введите корректную минуту (от 0 до 59).")
-
     elif step_info["step"] == 7:  # запрос индекса для удаления
         try:
             action_id = message.text
@@ -254,7 +278,168 @@ def handle_user_input(message):
                 bot.send_message(message.chat.id, "Действие с таким ID не найдено. Пожалуйста, введите корректный ID:")
         except Exception as e:
             bot.send_message(message.chat.id, f"Произошла ошибка: {str(e)}. Пожалуйста, попробуйте еще раз.")
+    elif step_info["step"] == 8:  # добавляем минуты в файл
+        try:
+            # Пытаемся преобразовать текст сообщения в число (минуты)
+            trigger = int(message.text)
+            # Проверяем, что минуты в допустимом диапазоне (0-59)
+            if not (0 <= trigger <= 100):
+                raise ValueError
+            # Получаем сохраненные ранее часы
+            #hour = step_info["hour"]
+            # Получаем или создаем запись пользователя в user_states
+            user_data = user_states.setdefault(user_id, {"light": False, "watering": False, "actions": {}, "notifications": {}})
+            # Получаем или создаем словарь действий пользователя
+            notifications = user_data.setdefault("notifications", {})
+            # Генерируем новый ID для действия (максимальный существующий + 1)
+            next_id = str(max([int(k) for k in notifications.keys()] + [0]) + 1)
+            # Создаем новое действие полива с указанными параметрами
+            notifications[next_id] = {
+                "type": "humidity",  # Тип действия - полив
+                "trigger": trigger,  # Состояние - включено
+            }
+            # Сохраняем изменения в файл
+            save_states()
+            # Отправляем пользователю подтверждение с ID действия
+            bot.send_message(message.chat.id, f"Сохранено как действие #{next_id}.")
+            # Удаляем пользователя из списка находящихся в процессе ввода
+            user_input_steps.pop(user_id)
+        except ValueError:
+            # В случае ошибки просим ввести корректное значение
+            bot.send_message(message.chat.id, "Пожалуйста, введите целое число от 0 до 100.")
+    elif step_info["step"] == 9:  # добавляем минуты в файл
+        try:
+            # Пытаемся преобразовать текст сообщения в число (минуты)
+            trigger = int(message.text)
+            # Проверяем, что минуты в допустимом диапазоне (0-59)
+            if not (0 <= trigger <= 100):
+                raise ValueError
+            # Получаем сохраненные ранее часы
+            #hour = step_info["hour"]
+            # Получаем или создаем запись пользователя в user_states
+            user_data = user_states.setdefault(user_id, {"light": False, "watering": False, "actions": {}, "notifications": {}})
+            # Получаем или создаем словарь действий пользователя
+            notifications = user_data.setdefault("notifications", {})
+            # Генерируем новый ID для действия (максимальный существующий + 1)
+            next_id = str(max([int(k) for k in notifications.keys()] + [0]) + 1)
+            # Создаем новое действие полива с указанными параметрами
+            notifications[next_id] = {
+                "type": "temperature",  # Тип действия - полив
+                "trigger": trigger,  # Состояние - включено
+            }
+            # Сохраняем изменения в файл
+            save_states()
+            # Отправляем пользователю подтверждение с ID действия
+            bot.send_message(message.chat.id, f"Сохранено как действие #{next_id}.")
+            # Удаляем пользователя из списка находящихся в процессе ввода
+            user_input_steps.pop(user_id)
+        except ValueError:
+            # В случае ошибки просим ввести корректное значение
+            bot.send_message(message.chat.id, "Пожалуйста, введите целое число от 0 до 100.")
+    elif step_info["step"] == 10:  # добавляем минуты в файл
+        try:
+            # Пытаемся преобразовать текст сообщения в число (минуты)
+            trigger = int(message.text)
+            # Проверяем, что минуты в допустимом диапазоне (0-59)
+            if not (0 <= trigger <= 100):
+                raise ValueError
+            # Получаем сохраненные ранее часы
+            #hour = step_info["hour"]
+            # Получаем или создаем запись пользователя в user_states
+            user_data = user_states.setdefault(user_id, {"light": False, "watering": False, "actions": {}, "notifications": {}})
+            # Получаем или создаем словарь действий пользователя
+            notifications = user_data.setdefault("notifications", {})
+            # Генерируем новый ID для действия (максимальный существующий + 1)
+            next_id = str(max([int(k) for k in notifications.keys()] + [0]) + 1)
+            # Создаем новое действие полива с указанными параметрами
+            notifications[next_id] = {
+                "type": "illumination",  # Тип действия - полив
+                "trigger": trigger,  # Состояние - включено
+            }
+            # Сохраняем изменения в файл
+            save_states()
+            # Отправляем пользователю подтверждение с ID действия
+            bot.send_message(message.chat.id, f"Сохранено как действие #{next_id}.")
+            # Удаляем пользователя из списка находящихся в процессе ввода
+            user_input_steps.pop(user_id)
+        except ValueError:
+            # В случае ошибки просим ввести корректное значение
+            bot.send_message(message.chat.id, "Пожалуйста, введите целое число от 0 до 100.")
+    elif step_info["step"] == 11:  # добавляем минуты в файл
+        try:
+            # Пытаемся преобразовать текст сообщения в число (минуты)
+            trigger = int(message.text)
+            # Проверяем, что минуты в допустимом диапазоне (0-59)
+            if not (0 <= trigger <= 100):
+                raise ValueError
+            # Получаем сохраненные ранее часы
+            #hour = step_info["hour"]
+            # Получаем или создаем запись пользователя в user_states
+            user_data = user_states.setdefault(user_id, {"light": False, "watering": False, "actions": {}, "notifications": {}})
+            # Получаем или создаем словарь действий пользователя
+            notifications = user_data.setdefault("notifications", {})
+            # Генерируем новый ID для действия (максимальный существующий + 1)
+            next_id = str(max([int(k) for k in notifications.keys()] + [0]) + 1)
+            # Создаем новое действие полива с указанными параметрами
+            notifications[next_id] = {
+                "type": "water_level",  # Тип действия - полив
+                "trigger": trigger,  # Состояние - включено
+            }
+            # Сохраняем изменения в файл
+            save_states()
+            # Отправляем пользователю подтверждение с ID действия
+            bot.send_message(message.chat.id, f"Сохранено как действие #{next_id}.")
+            # Удаляем пользователя из списка находящихся в процессе ввода
+            user_input_steps.pop(user_id)
+        except ValueError:
+            # В случае ошибки просим ввести корректное значение
+            bot.send_message(message.chat.id, "Пожалуйста, введите целое число от 0 до 100.")
+    elif step_info["step"] == 30:  # запрос индекса для удаления
+        try:
+            action_id = message.text
+            # Проверяем, существует ли такое действие у пользователя
+            if user_id in user_states and action_id in user_states[user_id].get("notifications", {}):
+                # Удаляем действие
+                del user_states[user_id]["notifications"][action_id]
+                save_states()  # сохраняем изменения в файл
+                bot.send_message(message.chat.id, f"Действие #{action_id} успешно удалено.")
+                user_delete_steps.pop(user_id)  # удаляем пользователя из процесса удаления
+            else:
+                bot.send_message(message.chat.id, "Триггер с таким ID не найдено. Пожалуйста, введите корректный ID:")
+        except Exception as e:
+            bot.send_message(message.chat.id, f"Произошла ошибка: {str(e)}. Пожалуйста, попробуйте еще раз.")
 
+def send_notifications(message):
+    user_id = str(message.chat.id)
+
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("добавить триггер уведомлений", callback_data="custom_1"))
+    markup.add(types.InlineKeyboardButton("удалить триггер", callback_data="custom_2"))
+
+    user_data = user_states.get(user_id, {})
+    notifications = user_data.get("notifications", {})
+    notifications_text = "*Уведомления:*\n\n"
+    for note_id, note in notifications.items():
+        type_emoji = {
+            "humidity": "💧 Влажность",
+            "temperature": "🌡 Температура",
+            "illumination": "🔆 Освещённость",
+            "water_level": "🚰 Уровень воды"
+        }
+        note_type = type_emoji.get(note["type"], note["type"])
+        trigger = note["trigger"]
+        notifications_text += f"• #{note_id} — {note_type}, триггер: {trigger}\n\n"
+
+    bot.send_message(message.chat.id, notifications_text, parse_mode='Markdown', reply_markup=markup)
+
+def add_notifications_buttons(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("влажность", callback_data="custom_11"))
+    markup.add(types.InlineKeyboardButton("температура", callback_data="custom_12"))
+    markup.add(types.InlineKeyboardButton("освещенность", callback_data="custom_13"))
+    markup.add(types.InlineKeyboardButton("уровень воды", callback_data="custom_14"))
+
+    bot.send_message(message.chat.id, "выберите параметр для триггера:", parse_mode='Markdown', reply_markup=markup)
 
 def send_settings(message):
     user_id = str(message.chat.id)
